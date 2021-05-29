86ca93d45c4d873bb1db92a2b970d18540a58be5
gupta-riya <reeya211999@gmail.com>
2021-05-29 14:17:46 +0530

    open file on the way

--- activity/newOpenDownload.js
+++ activity/newOpenDownload.js
@@ -9,16 +9,47 @@ newFile.addEventListener("click",function(){
         location.reload();
         
     }
-
-
-
 })
 
 
 
+// let openFile = document.querySelector(".open");
+// openFile.addEventListener("click",function(){
+//     document.getElementById('files').addEventListener('change', function(evt){
+//         var reader = new FileReader();
+//         reader.readAsText(evt.target.file);
+//         reader.onload = function(theFile)
+//         {
+//             json = JSON.parse(e.target.result);
+// 			alert('json global var has been set to parsed json of this file here it is unevaled = \n' + JSON.stringify(json));
+//         }
+//         json = JSON.parse(e.target.result);
+//     });
+// })
 
+let openFile = document.querySelector(".open");
+openFile.addEventListener("click",uploadJsonFile);
+  
+function uploadJsonFile()
+{
+    function onChange(event) {
+        console.log("onchange");
+        var reader = new FileReader();
+        reader.onload = onReaderLoad;
+        reader.readAsText(event.target.files[0]);
+    }
 
+    function onReaderLoad(event){
 
+        console.log("readeingMode")
+        console.log(event.target.result);
+        var obj = JSON.parse(event.target.result);
+        console.log(obj[0][1][1].bold);
+    }
+    
+    document.getElementById('files').addEventListener('change', onChange);
+
+}
 
 
 
